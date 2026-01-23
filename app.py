ec35b7119faf4ab71abd124619047ef62253da03
Lavich <ishurygin@mmuz.lan>
2026-01-23 11:52:43 +0500

    feat: add database models

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from certificate-summary!")
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.api.main_router import router
+from src.database.db import delete_tables, create_tables
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Включение")
+    # await delete_tables()
+    # print("База очищена")
+    # await create_tables()
+    # print("База готова")
+    yield
+    print("Выключение")
+
+app = FastAPI(lifespan=lifespan, docs_url="/api/docs")
+
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
+app.include_router(router)
---
+++ src/database/db.py
@@ -0,0 +1,80 @@
+import datetime
+import uuid
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy import create_engine
+from typing import Annotated
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
+from sqlalchemy import text
+
+from config import settings
+
+engine = create_async_engine(settings.DB_URL_ASYNC)
+# engine_sync = create_engine(settings.DB_URL_SYNC)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+# Session = sessionmaker(bind=engine_sync)
+
+uidpk = Annotated[uuid.UUID, mapped_column(primary_key=True)]
+created_at = Annotated[
+    datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))
+]
+updated_at = Annotated[
+    datetime.datetime,
+    mapped_column(
+        server_default=text("TIMEZONE('utc', now())"), onupdate=datetime.datetime.utcnow
+    ),
+]
+is_delete = Annotated[bool, mapped_column(nullable=False, default=False)]
+
+
+# ------------------------------------------------
+# Models
+
+
+class BaseOrmModel(DeclarativeBase):
+    pass
+
+
+class User(BaseOrmModel):
+    __tablename__ = "user"
+
+    id: Mapped[uidpk]
+    username: Mapped[str]
+    isAdmin: Mapped[bool]
+
+
+class Certificate(BaseOrmModel):
+    __tablename__ = "certificate"
+
+    id: Mapped[uidpk]
+    cert_center: Mapped[str | None]
+    fio: Mapped[str | None]
+    post: Mapped[str | None]
+    organization: Mapped[str | None]
+    expire_date: Mapped[datetime.datetime | None]
+    status: Mapped[str | None]
+    comment: Mapped[str | None]
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+    is_delete: Mapped[is_delete]
+
+
+# ------------------------------------------------
+# Util functions
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseOrmModel.metadata.create_all)
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseOrmModel.metadata.drop_all)
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
+
