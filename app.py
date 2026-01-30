08385e0d8e630fe21384720af828a1d6e488c4ba
Lavich <ishurygin@mmuz.lan>
2026-01-30 11:01:11 +0500

    build: d-compose v5

--- app.py
+++ app.py
@@ -9,29 +9,36 @@ from src.services.user import UserService
 from src.utils.redis_client import get_redis
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Включение")
-    await delete_tables()
-    print("База очищена")
-    await create_tables()
-    print("База готова")
-    service = UserService(UserRepository)
-    await service.createAdmin()
-    print("Администратор установлен")
-    redis = await get_redis()
-    await redis.ping()
-    print("Redis подключен")
-    yield
-    await redis.close()
-    print("Выключение")
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     print("Включение")
+#     await delete_tables()
+#     print("База очищена")
+#     await create_tables()
+#     print("База готова")
+#     service = UserService(UserRepository)
+#     await service.createAdmin()
+#     print("Администратор установлен")
+#     redis = await get_redis()
+#     await redis.ping()
+#     print("Redis подключен")
+#     yield
+#     await redis.close()
+#     print("Выключение")
 
 
-app = FastAPI(lifespan=lifespan, docs_url="/api/docs")
+# app = FastAPI(lifespan=lifespan, docs_url="/api/docs")
+
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, allow_origins=["http://localhost:3000"], allow_methods=["*"], allow_headers=["*"],
     allow_credentials=True
 )
 
-app.include_router(router)
+# app.include_router(router)
+
+
+@app.get("")
+async def home():
+    return "HOME"
